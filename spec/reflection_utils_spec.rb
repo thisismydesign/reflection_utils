c4917fd005bc201431b4d8b857baca93e17c48c3
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-08 18:51:48 +0200

    Add #call test cases

--- spec/reflection_utils_spec.rb
+++ spec/reflection_utils_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ReflectionUtils do
     expect(ReflectionUtils::VERSION).not_to be nil
   end
 
-  before :all do
+  before :each do
     @test_value = 5
 
     class Example
@@ -14,7 +14,7 @@ RSpec.describe ReflectionUtils do
         @value = value
       end
 
-      def method
+      def method(msg)
         @value
       end
     end
@@ -36,4 +36,59 @@ RSpec.describe ReflectionUtils do
       end
     end
   end
+
+  describe "#call" do
+    context "Given a Method" do
+      it "Will call the method" do
+        method = ReflectionUtils.get_bound_instance_method(instance: @test_instance, method_name: :method)
+        expect(method).to receive(:call)
+        ReflectionUtils.call(method)
+      end
+    end
+
+    context "Given a Method with parameters" do
+      it "Will call the Method with parameters" do
+        method = ReflectionUtils.get_bound_instance_method(instance: @test_instance, method_name: :method)
+        params = 'hi'
+        expect(method).to receive(:call).with(params)
+        ReflectionUtils.call(method, params)
+      end
+    end
+
+    context "Given a lambda" do
+      it "Will call the method" do
+        callback = lambda {}
+        expect(callback).to receive(:call)
+        ReflectionUtils.call(callback)
+      end
+    end
+
+    context "Given a lambda with parameters" do
+      it "Will call the lambda with parameters" do
+        params = 'hi'
+        callback = lambda { |msg| }
+        expect(callback).to receive(:call).with(params)
+        ReflectionUtils.call(callback, params)
+      end
+    end
+
+    context "Given a Proc" do
+      it "Will call the Proc" do
+        callback = Proc.new {}
+        expect(callback).to receive(:call)
+        ReflectionUtils.call(callback)
+      end
+    end
+
+    context "Given a Proc with parameters" do
+      it "Will call the Proc with parameters" do
+        params = 'hi'
+        callback = Proc.new { |msg| }
+        expect(callback).to receive(:call).with(params)
+        ReflectionUtils.call(callback, params)
+      end
+    end
+  end
+
+  
 end
