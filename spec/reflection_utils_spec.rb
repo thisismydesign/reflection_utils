583d1b34e18c734af28f4e8f021a9a6ed075789b
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-08 18:38:31 +0200

    Add get_bound_instance_method; Remove placeholder test case

--- spec/reflection_utils_spec.rb
+++ spec/reflection_utils_spec.rb
@@ -5,7 +5,35 @@ RSpec.describe ReflectionUtils do
     expect(ReflectionUtils::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  before :all do
+    @test_value = 5
+
+    class Example
+
+      def initialize(value)
+        @value = value
+      end
+
+      def method
+        @value
+      end
+    end
+
+    @test_instance = Example.new(@test_value)
+  end
+
+  describe "#get_bound_instance_method" do
+    context "Given an instance and method name" do
+
+      it "Will return a method" do
+        method = ReflectionUtils.get_bound_instance_method(instance: @test_instance, method_name: :method)
+        expect(method).to be_a(Method)
+      end
+
+      it "Will return the method bound to the instance" do
+        method = ReflectionUtils.get_bound_instance_method(instance: @test_instance, method_name: :method)
+        expect(method.receiver).to eq(@test_instance)
+      end
+    end
   end
 end
