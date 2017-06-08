8cf7f8252931164511d512d6c9e79bde23c29729
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-08 18:32:44 +0200

    Add initial implementation

--- lib/reflection_utils.rb
+++ lib/reflection_utils.rb
@@ -1,5 +1,35 @@
-require_relative "reflection_utils/version"
-
 module ReflectionUtils
-  # Your code goes here...
+
+  def self.get_bound_instance_method(instance:, method_name:)
+    get_class_constant(instance: instance).instance_method(method_name).bind(instance)
+  end
+
+  def self.get_class_constant(instance:)
+    Object.const_get(instance.class.name)
+  end
+
+  def self.call(callback, params = nil)
+    params.nil? ? callback.call : callback.call(params)
+  end
+
+  def self.has_parameters?(proc, parameters)
+    parameters.each_with_index do |parameter, index|
+      return false unless has_parameter(proc, index, parameter)
+    end
+
+    true
+  end
+
+  def self.assert_parameters(proc, parameters)
+    parameters.each_with_index do |parameter, index|
+      raise ArgumentError, "Argument #{parameter} not found for #{proc} at #{index} index." unless has_parameter(proc, index, parameter)
+    end
+
+    true
+  end
+
+  def self.has_parameter(proc, parameter_index, parameter)
+    proc.parameters[parameter_index] == parameter
+  end
+
 end
