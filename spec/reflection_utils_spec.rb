68376ed82a2ff8156ebfd745ed549569315a2d11
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-10 23:57:57 +0200

    Add unit tests

--- spec/reflection_utils_spec.rb
+++ spec/reflection_utils_spec.rb
@@ -37,6 +37,17 @@ RSpec.describe ReflectionUtils do
     end
   end
 
+  describe "#get_class_constant" do
+    context "Given an instance" do
+
+      it "Will return instance's class object" do
+        clazz = ReflectionUtils.get_class_constant(instance: @test_instance)
+        expect(clazz).to be_a(Example.class)
+      end
+
+    end
+  end
+
   describe "#call" do
     context "Given a Method" do
       it "Will call the method" do
@@ -90,5 +101,56 @@ RSpec.describe ReflectionUtils do
     end
   end
 
-  
+  describe "#has_parameter?" do
+    context "Given a lambda / Proc / Method, parameter_index and parameter" do
+
+      it "Will return true if lambda has given parameter on given index" do
+        expect(ReflectionUtils.has_parameter?(lambda { |msg| }, 0, [:req, :msg])).to be true
+      end
+
+      it "Will return false if Proc has given parameter on different index" do
+        expect(ReflectionUtils.has_parameter?(Proc.new { |msg| }, 1, [:req, :msg])).to be false
+      end
+
+      it "Will return false if Method doesn't have given parameter" do
+        method = Example.instance_method(:method)
+        expect(ReflectionUtils.has_parameter?(method, 0, [:req, :something_else])).to be false
+      end
+      
+    end
+  end
+
+  describe "#has_parameters?" do
+    context "Given a lambda / Proc / Method and parameters" do
+      it "Will return if Method has given parameter" do
+        method = Example.instance_method(:method)
+        expect(ReflectionUtils.has_parameters?(method, [[:req, :msg]])).to be true
+      end
+
+      it "Will return if lambda has given parameters" do
+        expect(ReflectionUtils.has_parameters?(lambda { |a, b| }, [[:req, :a], [:req, :b]])).to be true
+      end
+
+      it "Will return if Proc has given parameters in given order" do
+        expect(ReflectionUtils.has_parameters?(Proc.new { |a, b| }, [[:opt, :b], [:opt, :a]])).to be false
+      end
+    end
+  end
+
+  describe "#assert_parameters" do
+    context "Given a lambda / Proc / Method and parameters" do
+      it "Will raise ArgumentError if lambda doesn't have given parameter" do
+        method = Example.instance_method(:method)
+        expect{ ReflectionUtils.assert_parameters(method, [[:req, :something_else]]) }.to raise_error ArgumentError
+      end
+
+      it "Will return true if lambda has given parameters" do
+        expect(ReflectionUtils.assert_parameters(lambda { |a, b| }, [[:req, :a], [:req, :b]])).to be true
+      end
+
+      it "Will raise ArgumentError if Proc doesn't have parameters in given order" do
+        expect{ ReflectionUtils.assert_parameters(Proc.new { |a, b| }, [[:opt, :b], [:opt, :a]]) }.to raise_error ArgumentError
+      end
+    end
+  end
 end
